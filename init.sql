092d45253a4b119bb32093f4845aac3cb5d8e30f
JF1020 <921020j@gmail.com>
2024-04-21 19:33:36 +0800

    update init

--- init.sql
+++ init.sql
@@ -31,16 +31,16 @@ CREATE TABLE Course (
 
 insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1223,"System Programing","IECS",3,"3-4","3-6",10,0,TRUE,3);
 insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1222,"OOP","IECS",3,"1-1","1-3",10,4,FALSE,2);
-insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1250,"Program Design","IECS",3,"4-5","4-7",15,12,TRUE,2);
+insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1250,"Program Design","IECS",3,"4-5","4-7",15,13,TRUE,2);
 insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1249,"Program Design","IECS",3,"3-2","3-4",15,8,TRUE,1);
 insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1344,"Digital Design","EE",3,"4-1","4-3",15,10,TRUE,1);
-insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1355,"Digital design lab","EE",3,"1-1","1-3",20,10,TRUE,1)
+insert into Course(Course_ID,Course_Name,Department,Credit,Timebegin,Timeend,Max_Capacity,Enrolled_Students,Mandatory,Grade) values(1355,"Digital design lab","EE",3,"1-1","1-3",20,10,TRUE,1);
 
 
 
 
 CREATE TABLE Enrollment (
-    --Enrollment_ID INT AUTO_INCREMENT PRIMARY KEY,
+    
     Student_ID VARCHAR(255),
     Course_ID INT,
     FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
